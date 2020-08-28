5a99fa1d8d2dbc1b5ba854a45caf96fabe365070
soleilyasmina <mounlasolomon@gmail.com>
2020-08-28 12:36:15 -0400

    create get request, acquire records from airtable, load on screen with titles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      const airtableURL = `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}/frylife?Grid%20View`;
+      const response = await axios.get(airtableURL, {
+        headers: {
+          'Authorization': `Bearer ${process.env.REACT_APP_AIRTABLE_KEY}`,
+        },
+      });
+      setReviews(response.data.records);
+    }
+    getReviews();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        reviews.map((review) => (
+          <h4>{review.fields.title}</h4>
+        ))
+      }
     </div>
   );
 }
